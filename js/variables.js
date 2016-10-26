a18d31e30a13455ea39fe9da9f188ccfdd232307
Eve Weinberg <evejweinberg@gmail.com>
2016-10-26 09:10:25 -0400

    override

--- js/variables.js
+++ js/variables.js
@@ -1,23 +1,4 @@
 // var enterEC;
-console.log('variables loaded')
-
-
 var scene1Transition = false;
-
-
-var camera, scene, renderer, group;
-var mobile = false;
-var mouseEvent = {
-    screenX: 0,
-    screenY: 0
-};
-var raycaster = new THREE.Raycaster();
-var target = new THREE.Object3D();
-var orgGeometry, object
-var pointLight, light2;
-var firstTube;
-var cameraTarget;
-
-
 var frames;
 var currentScene = 0;
