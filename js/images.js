705aa00924ade180b0c763d753e2924c05f8970a
Eve Weinberg <evejweinberg@gmail.com>
2016-10-26 00:34:19 -0400

    class tom

--- js/images.js
+++ js/images.js
@@ -1,6 +1,6 @@
 var images = ['img/2.jpg',
 
-'img/1.png', 'img/3.png', 'img/4.jpg','img/5b.jpg','img/6.jpg','img/7b.jpg','img/8.jpg','img/9.png','img/12b.jpg','img/13.jpg','img/20.PNG'
+'img/1.png', 'img/3.png', 'img/4.jpg','img/5b.jpg','img/6.jpg','img/7b.jpg','img/8.jpg','img/9.png','img/12b.jpg','img/13b.jpg','img/20.PNG'
 ]
 
 var audio = [ 'audio/closer.wav',
