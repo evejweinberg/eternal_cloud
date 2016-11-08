2e33c290583590eb3feafd924f54731f15c1f059
Eve Weinberg <eveweinberg@Eves-MacBook-Pro.local>
2016-11-08 10:46:11 -0500

    custom ascii shader

--- js/AsciiEffect.js
+++ js/AsciiEffect.js
@@ -11,7 +11,7 @@ THREE.AsciiEffect = function ( renderer, charSet, options ) {
 
 	// its fun to create one your own!
 
-	charSet = ( charSet === undefined ) ? ' .:-=+*#%@' : charSet;
+	charSet = ( charSet === undefined ) ? ' .✌☂☮-=♡☆#♥█' : charSet;
 
 	// ' .,:;=|iI+hHOE#`$';
 	// darker bolder character set from https://github.com/saw/Canvas-ASCII-Art/
