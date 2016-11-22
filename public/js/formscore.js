58194f3249402385a6c255acbfac1d821285274e
Evejweinberg <evejweinberg@gmail.com>
2016-11-22 17:38:14 -0500

    making sure popUps branch is up to date

--- public/js/formscore.js
+++ public/js/formscore.js
@@ -44,7 +44,7 @@ jQuery("#CareerForm").submit(function(e){
   $('.progress-bar').attr("style","width:60%");
 // $("#candidateForm").fadeOut()
 $('#IntelligenceForm').fadeIn();
-$('#IntelligenceForm').fadeIn();
+
   var career = $(".career:checked").val();
 
   jQuery.ajax({
@@ -116,27 +116,38 @@ $('#done').fadeIn();
 
 })
 
-
+// jQuery("#percentageForm").submit(function(e){
+//
+//   jQuery.ajax({
+//   	url : '/api/update/'+personId,
+//   	dataType : 'json',
+//   	type : 'POST',
+//   	// we send the data in a data object (with key/value pairs)
+//   	data : {
+//   		percentIncome: percentIncome,
+//   	},
+//   	success : function(response){
+// 	  		// success
+//         console.log('success in posting intelligence')
+// 	  		console.log(response);
+// 	  		// now, clear the input fields
+// 	  		// jQuery("#candidateForm input").val('');
+//   	},
+//   	error : function(err){
+//   		// do error checking
+//   		alert("something went wrong");
+//   		console.error(err);
+//   	}
+//   });
+//
+//   // prevents the form from submitting normally
+//   e.preventDefault();
+//   return false;
+//
+// })
 
 
 jQuery("#IntelligenceForm").submit(function(e){
-
-  console.log('done forst one')
-  $('.progress-bar').attr("style","width:80%");
-// $("#candidateForm").fadeOut()
-$('#ActivismForm').fadeIn();
-
-  console.log(typeof $("#intelligence").val())
-  // if (typeof $("#intelligence").val() === String){
-      var intelligence = $("#intelligence").val();
-      // console.log(intelligence + 'is the intelligence')
-  // } else {
-    // intelligence = "null"
-    // console.log(intelligence + 'is the intelligence')
-  // }
-
-  // console.log(career + '  is value of career')
-  // console.log('updating career of person: '+ personId)
 
   jQuery.ajax({
   	url : '/api/update/'+personId,
@@ -145,6 +156,7 @@ $('#ActivismForm').fadeIn();
   	// we send the data in a data object (with key/value pairs)
   	data : {
   		intelligence: intelligence,
+      educationlevel: educationlevel
   	},
   	success : function(response){
 	  		// success
